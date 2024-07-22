a6077fae4716f314d20a134283f1c1ddd9f3d6be
David Hdez <davyd2h@gmail.com>
2024-07-22 06:24:27 -0500

    se utiliza fs.writeFile para escribir y fs.unlink para borrar o desvincular. fs.readFile para leer archivos

--- cursoEcmascript/node/modulos/fs.js
+++ cursoEcmascript/node/modulos/fs.js
@@ -4,8 +4,26 @@ const fs = require('fs'); // nos permite acceder a archivos, modificarlos, leerl
 function leer(ruta,cb) {
     fs.readFile(ruta, (err, data) => {
         //leído
-        console.log(data.toString())
+        cb(data.toString())
     })
 }
 
-leer(__dirname + '/archivo.txt')
+//leer(__dirname + '/archivo.txt', console.log)
+
+function escribir(ruta, contenido, cb){
+    fs.writeFile(ruta, contenido, function(err){
+        if(err){
+            console.log(`No he podido escribirlo ${err}`)
+        }else{
+            console.log('Se escribió correctamente')
+        }
+    })
+}
+escribir(__dirname + '/archivo1.txt', 'Soy un archivo nuevo')
+leer(__dirname + '/archivo.txt', console.log)
+
+// funcion para borrar 
+function borrar (ruta, cb){
+    fs.unlink(ruta, cb)
+}
+borrar(__dirname + '/archivo1.txt', console.log)
