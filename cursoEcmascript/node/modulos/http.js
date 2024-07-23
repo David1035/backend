ef3acfb272ac7be0be97c8c638cd0c370157b962
David Hdez <davyd2h@gmail.com>
2024-07-23 07:04:33 -0500

    trabajando con http y realizando peticiones

--- cursoEcmascript/node/modulos/http.js
+++ cursoEcmascript/node/modulos/http.js
@@ -1,4 +1,27 @@
-const http = requiere('http')
-http.createServer(function(){
-    
-})
+const http = require('http')
+
+http.createServer(router).listen(3100);
+
+function router (req, res){
+    console.log('Nueva petición!');
+    console.log(req.url);
+
+    switch (req.url){
+        case '/hola':
+            res.write('hola, que tal');
+            res.end();
+            break;
+        default:
+            res.write('Error 4004: No se lo que quieres')
+            res.end();
+    }
+/*
+    res.writeHead(201, { 'Content-Type': 'text/plain' })
+    // rspuesta al usuario
+    res.write('Hola, ya se usar HTTP de NodeJS')
+
+    res.end();
+    // los puertos más comunes 3000 */
+}
+
+console.log('escuchando en el puerto 3000')
