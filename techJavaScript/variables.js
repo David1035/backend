84b7f6da95ec72bd32fe6b5b8f220b40a8811efe
David H <davyd2h@gmail.com>
2024-08-06 06:12:41 -0500

    cambios ok

--- techJavaScript/variables.js
+++ techJavaScript/variables.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const textarea = document.querySelector('.input-observaciones');
+
+    textarea.addEventListener('input', function() {
+        this.style.height = 'auto'; // Reset the height
+        this.style.height = this.scrollHeight + 'px'; // Set the height based on the content
+    });
+});
+
+
+<textarea id="observaciones" class="input-observaciones"></textarea>
 
 
 /* 
