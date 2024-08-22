c6c21465df23089d1f5a68e795edf52805f9a753
David Hdez <davyd2h@gmail.com>
2024-08-22 07:27:42 -0500

    estructura del proyecto completa

---
+++ proyectoMySql/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import conectarDB from './configDB/db.js';
+import citasRoutes from './routes/RoutesCitas.js'
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/citas', citasRoutes)
+
+//verificamos la conexión a la base de datos
+try {
+    await conectarDB.authenticate();
+    console.log('conexión a la base de datos con éxito')
+} catch (error) {
+    console.error('tenemos un error al conectar a base de datos', error)
+}
+
+app.get('/', (req, res) => {
+    res.send('HOLA MUNDO')
+})
+
+app.listen(5000, () => {
+    console.log('El servidor está corriendo en http://localhost:5000/')
+})
+
---
+++ proyectoMySql/models/Citas.js
@@ -0,0 +1,31 @@
+import conectarDB from '../configDB/db.js';
+import { DataTypes } from 'sequelize';;
+
+const Citas = conectarDB.define('citas', {
+    nombre_medico:
+    {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    especialidad:
+    {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    fecha:
+    {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    duracion:
+    {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+})
+
+export default Citas;
