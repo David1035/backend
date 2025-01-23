9c6a5872cde44813e133eddd2028775884b3b2ea
David H <davyd2h@gmail.com>
2025-01-22 21:24:31 -0500

    manejador de eventos, crear tarea

---
+++ ManipulacionDelDOMjs/app.js
@@ -0,0 +1,34 @@
+const taskForm = document.getElementById('task-form');
+const taskList = document.getElementById('task-list');
+
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskInput = document.getElementById('task-input');
+    const task = taskInput.value;
+    console.log(task)
+    if(task){
+        taskList.append(createTaskElement(task))
+        taskInput.value = ''
+    }
+})
+
+function createTaskElement(task) {
+    const li = document.createElement('li')
+    li.textContent = task;
+
+    //botones
+    li.append(createButton('❌', 'delete-btn'), createButton('✏️', 'edit-btn')) 
+    return li;
+}
+
+function createButton(text, className) {
+    const btn = document.createElement('span')
+    btn.textContent = text;
+    btn.className = className;
+    btn.style.cursor = 'pointer';
+    return btn;
+}
+
+taskList.addEventListener('click', (event) => {
+    console.log(event.target)
+})
