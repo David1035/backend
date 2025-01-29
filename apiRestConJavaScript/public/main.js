85e261a8f4500b9a9e16d28d4f0865ef2179cebb
David H <davyd2h@gmail.com>
2025-01-29 15:48:33 -0500

    se confirma código ok, funcional

--- apiRestConJavaScript/public/main.js
+++ apiRestConJavaScript/public/main.js
@@ -1,25 +1,61 @@
+//const { response } = require("express");
+
 const nameSpan = document.getElementById('nameSpan');
 const ageSpan = document.getElementById('ageSpan');
-const actualizarBtn = document.getElementById('actualizar-btn')
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
+const I_AM_KEY = 'api_key=live_1344OCleEw0ckKke6F79UQySZeZSYxzGwz9kJ2krCRhoC4kGq1rjBkJRo9crGDQj'
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?';
+const spanError = document.getElementById('spanError')
 
 
-async function getGatitos () {
+
+async function getLoadMichis () {
     try {
-        const response = await fetch(API_URL);
+        const response = await fetch(API_URL_RANDOM);
         const data = await response.json()
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
-        const img3 = document.getElementById('img3');
 
         img1.src = data[0].url
         img2.src = data[1].url
-        img3.src = data[2].url
 
     } catch (error) {
         console.error('se presenta error al recibir los datos ')
     }
 }
 
-actualizarBtn.addEventListener('click', getGatitos)
-getGatitos()    
+async function getFavoritesMichis () {
+    try {
+        const response = await fetch(API_URL_FAVORITES, {
+            headers: { "x-api-key": I_AM_KEY },
+        });
+        console.log(response)
+        if(response.status !== 200) {
+            spanError.innerHTML = 'Se presentó un error ' + response.status + data.message
+        }
+        const data = await response.json()
+        console.log('Favorites')
+        console.log(data)
+
+
+    } catch (error) {
+        console.error('no carga datos')
+    }
+}
+
+async function saveFavourite() {
+    const res = await fetch(API_URL_FAVORITES, {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            image_id: 'adz'
+        })
+    })
+    console.log('save')
+    console.log(res)
+}
+   
+getFavoritesMichis()
+saveFavourite()
