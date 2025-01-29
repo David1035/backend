3dff35f4283c7b806467cf35fe056e112f3e8a41
David H <davyd2h@gmail.com>
2025-01-29 11:23:22 -0500

    maquetación inicial del proyecto

--- apiRestConJavaScript/public/main.js
+++ apiRestConJavaScript/public/main.js
@@ -1,29 +1,21 @@
 const nameSpan = document.getElementById('nameSpan');
 const ageSpan = document.getElementById('ageSpan');
 const actualizarBtn = document.getElementById('actualizar-btn')
-const URL = 'https://api.thecatapi.com/v1/images/search';
-const API_URL = 'http://localhost:5000/api/forms';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
-async function definirHora () {
-    try {
-        const response = await fetch(API_URL)
-        const data = await response.json()
-        const fechaCreada = new Date(data[0].createdAt)
-        const hora = new Date()
-        console.log(fechaCreada)
-        console.log(hora)
-    } catch (error) {
-        
-    }
-}
-definirHora()
 
 async function getGatitos () {
     try {
-        const response = await fetch(URL);
+        const response = await fetch(API_URL);
         const data = await response.json()
-        nameSpan.textContent = data[0].url;
-        ageSpan.src = data[0].url === 0 ? '' : data[0].url;
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
+        const img3 = document.getElementById('img3');
+
+        img1.src = data[0].url
+        img2.src = data[1].url
+        img3.src = data[2].url
+
     } catch (error) {
         console.error('se presenta error al recibir los datos ')
     }
