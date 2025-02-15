27a1831c9ad2a26db12e703c98f9d032ecfb3c3e
David H <davyd2h@gmail.com>
2025-02-15 07:55:33 -0500

    alcancía funcional con un closure

--- apiRestConJavaScript/app.js
+++ apiRestConJavaScript/app.js
@@ -1,9 +1,20 @@
 // closure
 
-
-
-
-
-function otraFuncion(){
-    let b = 0;
+function dinerLocal() {
+    let dinero = 0;
+    return function ahorrar(total) {
+        if(!total){
+            console.log('ingresa un valor a sumar');
+        } else {
+            dinero += total;
+            console.log(`El total de mi dinero almacenado es: $${dinero}`)
+        }
+    }
 }
+
+const alcancia = dinerLocal()
+
+alcancia()
+alcancia(25)
+alcancia(125)
+alcancia(125)
