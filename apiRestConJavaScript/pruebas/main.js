2a83ebff62f35b89d6984d1d46839fef8b101575
David H <davyd2h@gmail.com>
2025-02-16 20:04:17 -0500

    crear rutas con el constructor se ve relativamente sencillo

--- apiRestConJavaScript/pruebas/main.js
+++ apiRestConJavaScript/pruebas/main.js
@@ -1,102 +1,37 @@
-/** Un objeto literal en JS, aquí lo podemos ver y su funcionamiento */
-
-// const david = { // objeto literal
-//     name: 'David',
-//     age: 31,
-//     cursoAprobados: [
-//         'Curso definitivo HTML',
-//         'Curso JS'
-//     ],
-//     aprobarCurso (nuevoCurso) {
-//         this.cursoAprobados.push(nuevoCurso)
-//     },
-// };
-
-// david.cursoAprobados.push('Curso response Desing ')
-// david.name = 'David Hernandez';
-// david.aprobarCurso('Definitivo de matematicas')
-
-/**Crear función constructora */
-function Student(name, age, cursoAprobados){
-    this.name = name;
-    this.age = age;
-    this.cursoAprobados = cursoAprobados;
-    // this.aprobarCurso = function (nuevoCurso) {
-    //     this.cursoAprobados.push(nuevoCurso)
-    // una forma de trabajar
-    // }
-}
-
-//con la herramienta prototype se crea por fuera
-Student.prototype.aprobarCurso = function(nuevoCurso){
-    this.cursoAprobados.push(nuevoCurso)
-}
-
-const juanita = new Student(
-    'David Hernandez',
-    31,
-    [
-        'Curso de HTML',
-        'Curso de programacion'
-    ]
-)
-juanita.aprobarCurso('Curso de fisica')
-
-// Ptototipos con la sintaxis de las clases
-
-class Student2 {
+class Student {
     constructor({
+        name,
         email,
-        name, 
-        age, 
-        cursoAprobados = []
+        userName,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
     }) {
         this.name = name;
-        this.age = age;
         this.email = email;
-        this.cursoAprobados = cursoAprobados;      
-    }
-
-    aprobarCurso(nuevoCurso){
-        this.cursoAprobados.push(nuevoCurso)
+        this.userName = userName;
+        this.sosialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
     }
 }
 
-const miguelito = new Student2(
-    {
-        email: 'david@gamil.com',
-        name: 'David H', 
-        age: 35,
-    }
-)
+const david = new Student({
+    name: 'David',
+    userName: 'davis1023',
+    email: 'davyd2h@gmail.com',
+    twitter: 'davis29w8js'
+});
 
-miguelito.aprobarCurso('Desarrollo Web con JS')
-miguelito.aprobarCurso('Ciencias de la computación')
-
-/**Ventajas de la POO */
-const Juan1 = {
-    name: 'JuanDC',
-    username: 'juandc',
-    points: 100,
-    socialMedia: {
-        twitter: 'fjuandc',
-        instagram: 'juandc',
-        facebook: undefined,
-    },
-    approvedCourses: [
-        'Curso de hTML',
-        'Curso practico'
-    ],
-    learningPaths: [
-        {
-            name: 'Escuela de desarrollo Web',
-            courses: [
-                'Curso de hTML',
-                'Curso practico',
-                'curso de maquetacion',
-                'curso maquetacion'
-
-            ]
-        }
-    ]
-}
+const luz = new Student({
+    name: 'Luz',
+    userName: 'luzJ',
+    email: 'luzj@gmail.com',
+    twitter: 'davis29w8js'
+})
