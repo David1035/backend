97224f1f47fc8f8d91392bbe1ee689ba3d7dd247
David H <davyd2h@gmail.com>
2025-03-01 20:22:47 -0500

    ejercicio con array puth(), unshift(), pop(), shift() y .length

---
+++ apiRestConJavaScript/proyectoHTMLbasico/main.js
@@ -0,0 +1,10 @@
+let frutas = ["Manzana", "Banana", "Uva"];
+frutas.push('Pera')
+console.log(frutas)
+frutas.unshift("Limon")
+console.log(frutas)
+frutas.pop()
+console.log(frutas)
+frutas.shift()
+console.log(frutas)
+console.log(frutas.length)
