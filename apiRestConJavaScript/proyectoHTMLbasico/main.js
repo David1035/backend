6be764d2aa4848246876c06fba0ec3f85dde5b4d
David H <davyd2h@gmail.com>
2025-03-04 11:21:41 -0500

    numeros y filter, recude, map

--- apiRestConJavaScript/proyectoHTMLbasico/main.js
+++ apiRestConJavaScript/proyectoHTMLbasico/main.js
@@ -1,10 +1,9 @@
-let frutas = ["Manzana", "Banana", "Uva"];
-frutas.push('Pera')
-console.log(frutas)
-frutas.unshift("Limon")
-console.log(frutas)
-frutas.pop()
-console.log(frutas)
-frutas.shift()
-console.log(frutas)
-console.log(frutas.length)
+let num = [1, 2, 5, 7, 9, 15, 21, 25]
+let total =  num.map(num => (num * 5))
+console.log(total)
+total = num.filter( (num) => num > 10)
+console.log(total)
+total = num.reduce((acc, cur) => acc + cur, 0)
+console.log(total)
+console.log(``)
+
