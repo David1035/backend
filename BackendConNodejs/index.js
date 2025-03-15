c7cabc6190404d0327e010fee909f8902c159728
David H <davyd2h@gmail.com>
2025-03-14 22:09:51 -0500

    @faker se importa, para tener una libreria para trabajar con un ecommers, se pone limite y se crea un size

--- BackendConNodejs/index.js
+++ BackendConNodejs/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
+
 const app = express();
 const port = 3000;
 
+
 app.get('/users', (req, res) => {
-  res.send('Lista de usuarios')
+  const { limit, offset } = req.query;
+  if(limit && offset) {
+    res.json({ limit, offset})
+  } else {
+    res.json({message: 'No hay parámetros'})
+  }
+})
+
+app.get('/products', (req, res) => {
+  const { size } = req.query;
+  const limit = size || 5;
+  const productos = [];
+  for (let index = 0; index < limit; index++) {
+    productos.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
+    })
+  }
+  res.json(productos)
 })
 
 app.listen(port, () => {
